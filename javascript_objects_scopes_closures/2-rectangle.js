07e4d84e416ee17740fc790aa2df121a05170543
cvuakdgreat <cvuak19@gmail.com>
2023-09-13 23:31:32 +0100

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(w, h){
-
-		if (w <= 0 || h <= 0) {
-			pass
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+		  this.width = w;
+		  this.height = h;
+		} else {
+		  
+		  Object.create(null);
 		}
-		else {
-			this.width = w
-			this.height = h
-		}
-	}
-
+	  }
 };
 
 
